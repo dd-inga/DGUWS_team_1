a6cfd893a6327e1fd186e05968936157f9100e6a
holy0 <96487453+holy0@users.noreply.github.com>
2023-05-31 05:03:55 +0900

    vscode로 환경 구축

--- src/gui/CustomerMainFrame.java
+++ src/gui/CustomerMainFrame.java
@@ -25,9 +25,10 @@ public class CustomerMainFrame extends JFrame {
     private static boolean isAdminLoggedIn = false; // admin 페이지 로그인 성공 여부
     private int nextWaitingNumber = 0;
     private int waitingCountNumber;
-    
-    /* TODO : 현재 웨이팅 팀 수 보여주기
-            : 전화번호 11자리 입력 후에 인원 수 입력되도록 설정하기
+
+    /*
+     * TODO : 현재 웨이팅 팀 수 보여주기
+     * : 전화번호 11자리 입력 후에 인원 수 입력되도록 설정하기
      */
 
     // constructor
@@ -85,12 +86,12 @@ public class CustomerMainFrame extends JFrame {
 
         // 위치, 크기 설정
         titleLabel.setBounds(200, 10, 100, 20);
-        buttonPanel.setBounds(20,40,200,200);
-        phoneNumberLabel.setBounds(240,40,80,30);
+        buttonPanel.setBounds(20, 40, 200, 200);
+        phoneNumberLabel.setBounds(240, 40, 80, 30);
         phoneNumberTextField.setBounds(300, 40, 150, 30);
         countLabel.setBounds(240, 80, 80, 30);
         countTextField.setBounds(300, 80, 150, 30);
-        submitButton.setBounds(240,150,80,30);
+        submitButton.setBounds(240, 150, 80, 30);
         adminPageButton.setBounds(20, 300, 80, 30);
 
         // component 추가
@@ -112,8 +113,7 @@ public class CustomerMainFrame extends JFrame {
         if (!phoneNumber.isEmpty()) {
             long nowDate = System.currentTimeMillis();
             int waitingNumber = nextWaitingNumber++;
-            Customer customer = new Customer
-                    (waitingNumber, phoneNumber, count, "대기", new Timestamp(nowDate), false);
+            Customer customer = new Customer(waitingNumber, phoneNumber, count, "대기", new Timestamp(nowDate), false);
             customerList.addCustomer(customer);
             phoneNumberTextField.setText("");
             countTextField.setText("");
@@ -128,7 +128,7 @@ public class CustomerMainFrame extends JFrame {
             showAdminPage();
         }
     }
-    
+
     void showAdminPage() {
         adminFrame = new AdminMainFrame(this);
         adminFrame.setVisible(true);
@@ -147,8 +147,7 @@ public class CustomerMainFrame extends JFrame {
         public void actionPerformed(ActionEvent e) {
             JButton sourceButton = (JButton) e.getSource();
             String buttonText = sourceButton.getText();
-            phoneNumberTextField.setText
-                    (phoneNumberTextField.getText() + buttonText);
+            phoneNumberTextField.setText(phoneNumberTextField.getText() + buttonText);
         }
     }
 
